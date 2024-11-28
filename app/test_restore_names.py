264ad1ef3f4cceda610c1d9915576fba1a7f980c
Oleh <sholeg2005@gmail.com>
2024-11-29 00:52:36 +0100

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,35 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_for_restore_name_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+    ]
+    restore_names(users)
+    assert users == [
+  {
+    "first_name": "Jack",
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+},]
+
+
+def test_for_restore_name_without() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users == [
+  {
+    "first_name": "Mike",
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },]
